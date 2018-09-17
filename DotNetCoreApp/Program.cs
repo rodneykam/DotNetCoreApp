db63ed64b3309b185f04ec5f27bbe9a2b30027ed
Rodney Kam <>
2018-09-17 00:17:46 -0700

    Initial Version

---
+++ DotNetCoreApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DotNetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _vaultClientId = Environment.GetEnvironmentVariable("VaultClientId");
+            var _vaultClientKey = Environment.GetEnvironmentVariable("VaultClientKey");
+            var _vaultURI = Environment.GetEnvironmentVariable("VaultURI");
+            
+            Console.WriteLine(string.Format("Client Id: {0}", _vaultClientId));
+            Console.WriteLine(string.Format("Client Key: {0}", _vaultClientKey));
+            Console.WriteLine(string.Format("Client URI: {0}", _vaultURI));
+        }
+    }
+}
