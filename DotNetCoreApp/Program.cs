300236fd54f10dc7ddcb04abf9f1ea2db2940fd6
Rodney Kam <>
2018-09-18 18:08:48 -0700

    More forgot to save changes.

--- DotNetCoreApp/Program.cs
+++ DotNetCoreApp/Program.cs
@@ -12,11 +12,12 @@ namespace DotNetCoreApp
             var clientId = kvHelper.GetSecret("client-id"); 
             var clientKey = kvHelper.GetSecret("client-key");
             var tenantId = kvHelper.GetSecret("tenant-id");
-               
+
             Console.WriteLine(string.Format("subscriptionId: {0}", subscriptionId));
             Console.WriteLine(string.Format("clientId: {0}", clientId));
             Console.WriteLine(string.Format("clientKey: {0}", clientKey));
             Console.WriteLine(string.Format("tenantId: {0}", tenantId));
+
         }
     }
 }
